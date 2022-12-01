065b5746d02d9e983b7c05d8b6797fb22fe62abc
Demian Yovanovich <subwaydemiany@gmail.com>
2022-12-01 19:03:00 -0300

    agregado CRUD al controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,22 +1,49 @@
 class CharactersController < ApplicationController
+  before_action :set_character, only: %i[show edit update destroy]
+
   def index
+    @characters = Character.all
   end
 
   def show
   end
 
   def new
+    @character = Character.new
   end
 
   def create
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to character_path(@character)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @character.update(character_params)
+      redirect_to character_path(@character)
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @character.destroy
+    redirect_to character_path, status: :see_other
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :review, :actor, :image_url)
+  end
+
+  def set_character
+    @character = Character.find(params[:id])
   end
 end
