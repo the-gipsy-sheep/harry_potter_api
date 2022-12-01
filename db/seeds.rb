b02192092e8a27b8a5dda6e059952be24e76f2b1
Demian Yovanovich <subwaydemiany@gmail.com>
2022-12-01 19:51:44 -0300

    creado el seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,63 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Character.create(
+  name: 'Alastor "Ojoloco" Moody',
+  review: 'Uno de los mejores Aurors del ministerio, la mitad de Azkaban la llenó solo. Siempre prefirio capturar a sus objetivos con vida si fuera posible. Desconfiado y precavido siempre priorizó la seguridad de sus colegas',
+  actor: 'Brendan Gleeson',
+  image_url: "https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Alastor-Ojoloco-Moody__seldth.jpg
+  "
+)
+
+Character.create(
+  name: 'Cho Chang',
+  review: 'Buscadora en el equipo de Quidditch de Ravenclaw, fue el primer amor de Harry Potter',
+  actor: 'Katie Leung',
+  image_url: "https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Cho-Chang__etm4ip.jpg
+  "
+)
+
+Character.create(
+  name: 'Cedric Diggory',
+  review: 'Buscador y capitan del equipo de Quidditch de Hufflepuff, compitió en el torneo de los tres magos como campeón de Hogwarts',
+  actor: 'Robert Pattinson',
+  image_url: 'https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Cedric-Diggory__teeyto.jpg
+  '
+)
+
+Character.create(
+  name: 'Harry Potter',
+  review: 'Conocido por todos como "El niño que vivió" Harry se volvio famoso incluso antes de aprender a caminar. Junto a sus mejores amigos Ron y Hermione vivió muchas aventuras. ',
+  actor: 'Daniel Radcliffe',
+  image_url: "https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Harry-Potter__htwqje.jpg
+  "
+)
+
+Character.create(
+  name: 'Ron Weasly',
+  review: 'Mejor amigo de Harry Potter, humilde y leal, el sexto de 7 hermanos. El mejor jugador de ajedrez mágico de Hogwarts en mucho tiempo',
+  actor: 'Rupert Grint',
+  image_url: "https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Ron-Weasley__lyqtwc.jpg
+  "
+)
+
+Character.create(
+  name: 'Rubeus Hagrid',
+  review: 'Guardian de los terrenos de Hogwarts y profesor de cuidado de criaturas mágicas, Hagrid se destaca por su gran tamaño y su aún mayor corazón. Fue quien le reveló a Harry su identidad de mago',
+  actor: 'Robbie Coltrane',
+  image_url: "https://res.cloudinary.com/abdulaba/image/upload/v1669900364/Harry/Rubeus-Hagrid__izy7qd.jpg
+  "
+)
+
+Character.create(
+  name: 'Albus Dumbledore',
+  review: 'Según muchos opinan el mejor director que ha tenido Hogwarts. Principal organizador de la resistencia contra Quien No Debe Ser Nombrado',
+  actor: 'Michael Gambon',
+  image_url: "https://res.cloudinary.com/abdulaba/image/upload/v1669900364/Harry/Albus-Dumbledore__ztskyf.jpg
+  "
+)
+
+Character.create(
+  name: 'Hermione Granger',
+  review: 'Hija de muggles, es una de las hechiceras mas brillantes que se ha visto en mucho tiempo. Inteligente, recta, una de las aliadas mas importantes de Harry en su lucha contra el innombrable',
+  actor: 'Emma Watson',
+  image_url: "https://res.cloudinary.com/abdulaba/image/upload/v1669900364/Harry/Hermione-Granger__ze0mnx.jpg
+  "
+)
